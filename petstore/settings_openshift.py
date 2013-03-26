0b6da76875af56a8f2777c5ffadff9f7d9010295
José Moreira <zemanel@zemanel.eu>
2013-03-26 00:06:32 +0000

    updated openshift settings module to use postgres specific environment variables

--- petstore/settings_openshift.py
+++ petstore/settings_openshift.py
@@ -14,10 +14,10 @@ DATABASES = {
     'default': {
         'ENGINE'    : 'django.db.backends.postgresql_psycopg2',
         'NAME'      : os.environ['OPENSHIFT_APP_NAME'],
-        'USER'      : os.environ['OPENSHIFT_DB_USERNAME'],
-        'PASSWORD'  : os.environ['OPENSHIFT_DB_PASSWORD'],
-        'HOST'      : os.environ['OPENSHIFT_DB_HOST'],
-        'PORT'      : os.environ['OPENSHIFT_DB_PORT'],
+        'USER'      : os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],
+        'PASSWORD'  : os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'],
+        'HOST'      : os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
+        'PORT'      : os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'],
         }
 }
 
